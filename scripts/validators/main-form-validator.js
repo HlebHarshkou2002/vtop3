84595d57ce18301ecf7329a50ef1b912412498b3
HlebHarshkou2002 <erichkrause2012@tut.by>
2023-06-26 10:45:54 +0300

    added a submit button animation

--- scripts/validators/main-form-validator.js
+++ scripts/validators/main-form-validator.js
@@ -1,66 +1,95 @@
-let form = document.querySelector('.main-form'),
-    formInputs = document.querySelectorAll('.js-input'),
-    inputEmail = document.querySelector('.js-input-email'),
-    inputPassword = document.querySelector('.js-input-password'),
-    inputConfirmPassword = document.querySelector('.js-input-confirm-password'),
-    passwordErrorModals = document.querySelectorAll('.password-error-modal');
+let form = document.querySelector(".main-form"),
+	formInputs = document.querySelectorAll(".js-input"),
+	inputEmail = document.querySelector(".js-input-email"),
+	inputPassword = document.querySelector(".js-input-password"),
+	inputConfirmPassword = document.querySelector(".js-input-confirm-password"),
+	passwordErrorModals = document.querySelectorAll(".password-error-modal"),
+	submitButton = document.querySelector(".submit-button");
 
-
+console.log(submitButton);
 function validatePassword(password) {
-    let reg = /(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])/g;
-    return reg.test(String(password));
+	let reg = /(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])/g;
+	return reg.test(String(password));
 }
 
 function showPasswordErrorModals() {
-    passwordErrorModals.forEach(function (modal) {
-        modal.style.display = 'block';
-    })
+	passwordErrorModals.forEach(function (modal) {
+		modal.style.display = "block";
+	});
+}
+
+function animateErrorSubmit() {
+	submitButton.classList.add("animate-error-submit");
+	setTimeout(() => {
+		submitButton.classList.remove("animate-error-submit");
+	}, 100);
+}
+
+
+//Функция, которая вызывается после успешной регистрации
+function accessSubmit() {
+	//Очищает все поля формы
+	formInputs.forEach(function (input) {
+		input.value = "";
+	});
+	setTimeout(() => {
+		alert("Регистрация прошла успешно");
+	}, 500);
 }
 
 form.onsubmit = function () {
-    let emailVal = inputEmail.value,
-        passwordVal = inputPassword.value,
-        passwordConfirmVal = inputConfirmPassword.value,
-        emptyInputs = Array.from(formInputs).filter(input => input.value === '');
+	let emailVal = inputEmail.value,
+		passwordVal = inputPassword.value,
+		passwordConfirmVal = inputConfirmPassword.value,
+		emptyInputs = Array.from(formInputs).filter((input) => input.value === "");
 
-    formInputs.forEach(function (input) {
-        if (input.value === '') {
-            input.classList.add('error');
-        } else {
-            input.classList.remove('error');
-        }
-    })
+	//Проверка на пустые инпуты
+	formInputs.forEach(function (input) {
+		if (input.value === "") {
+			input.classList.add("error");
+			animateErrorSubmit();
+		} else {
+			input.classList.remove("error");
+		}
+	});
 
-    if (emptyInputs.length !== 0) {
-        // alert("Inputs not filled")
-        // return false;
-    }
+	//Проверка на то есть ли пустые инпуты
+	if (emptyInputs.length !== 0) {
+		// alert("Inputs not filled")
+		animateErrorSubmit();
+		// return false;
+	}
 
-    if (passwordVal.length < 8) {
-        inputPassword.classList.add('error');
-        showPasswordErrorModals();
-        return false;
-    } else {
-        console.log(passwordVal)
-        if (!validatePassword(passwordVal)) {
-            inputPassword.classList.add('error');
-            showPasswordErrorModals();
-            return false;
-        } else {
-            inputPassword.classList.remove('error');
-            passwordErrorModals.forEach(function (modal) {
-                modal.style.display = 'none';
-            })
-        }
-    }
+	//Валидация пароля
+	if (passwordVal.length < 8) {
+		inputPassword.classList.add("error");
+		showPasswordErrorModals();
+		animateErrorSubmit();
+		return false;
+	} else {
+		console.log(passwordVal);
+		if (!validatePassword(passwordVal)) {
+			inputPassword.classList.add("error");
+			showPasswordErrorModals();
+			animateErrorSubmit();
+			return false;
+		} else {
+			inputPassword.classList.remove("error");
+			passwordErrorModals.forEach(function (modal) {
+				modal.style.display = "none";
+			});
+		}
+	}
 
-    if (passwordVal !== passwordConfirmVal) {
-        inputConfirmPassword.classList.add('error');
-        return false;
-    } else {
-        inputConfirmPassword.classList.remove('error')
-    }
+	//Сравнения пароля и пароля введённого повторно
+	if (passwordVal !== passwordConfirmVal) {
+		inputConfirmPassword.classList.add("error");
+		animateErrorSubmit();
+		return false;
+	} else {
+		inputConfirmPassword.classList.remove("error");
+	}
 
-    console.log(formInputs)
-    return false;
-}
+	accessSubmit();
+	return false;
+};
